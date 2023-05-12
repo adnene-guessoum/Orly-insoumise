3e0dd608849b6434f0f256764fdb5268ac4e1e34
Adnene Guessoum <adnen.guessoum@gmail.com>
2023-05-13 01:30:30 +0200

    fix: footer layout bug

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,31 +15,33 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
   return (
     <>
       <Head customMeta={customMeta} />
-      <header>
-        <div className="bg-banner bg-cover bg-center h-59 max-w-5xl px-8 mx-auto">
-          <div className="flex items-center justify-between py-6">
-            <div className="back-wrapper bg-orange-50 dark:bg-cyan-950 ">
-              <Navigation />
-            </div>
-            <div className="back-wrapper bg-orange-50 dark:bg-cyan-950 ">
-              <ThemeSwitch />
+      <div className="min-h-screen">
+        <header>
+          <div className="bg-banner bg-cover bg-center h-59 max-w-5xl px-8 mx-auto">
+            <div className="flex items-center justify-between py-6">
+              <div className="back-wrapper bg-orange-50 dark:bg-cyan-950 ">
+                <Navigation />
+              </div>
+              <div className="back-wrapper bg-orange-50 dark:bg-cyan-950 ">
+                <ThemeSwitch />
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-      <main>
-        <div className="max-w-5xl px-8 py-4 mx-auto">{children}</div>
-      </main>
-      <footer className="py-8">
-        <div className="max-w-5xl px-8 mx-auto">
-          <a
-            className="text-gray-900 dark:text-white"
-            href="https://twitter.com/Actionpop_Orly"
-          >
-            Orly Insoumise - 2023
-          </a>
-        </div>
-      </footer>
+        </header>
+        <main>
+          <div className="max-w-5xl px-8 py-4 mx-auto">{children}</div>
+        </main>
+        <footer className="sticky top-[100vh] py-4">
+          <div className="max-w-5xl px-8 mx-auto">
+            <a
+              className="text-gray-900 dark:text-white"
+              href="https://twitter.com/Actionpop_Orly"
+            >
+              Orly Insoumise - 2023
+            </a>
+          </div>
+        </footer>
+      </div>
     </>
   );
 };
